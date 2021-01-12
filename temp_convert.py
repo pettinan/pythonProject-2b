8350f27befae0bcf652f30baa8ae88d1fb4dacdc
pettinan <pettinan@oregonstate.edu>
2021-01-12 13:48:05 -0800

    Project-2b

---
+++ temp_convert.py
@@ -0,0 +1,5 @@
+#author Noah Pettinato
+
+celsius = float(input('Enter temperature in Celsius: '))
+fahrenheit = (celsius * (9/5)) + 32
+print('%0.1f  Celsius is equal to %0.1f degree Fahrenheit' % (celsius, fahrenheit))
